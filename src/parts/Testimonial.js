3c42756269ff88988bc6bf719a33dd3d3b5fa7b0
yaelahno <rinosatyaputra.id@gmail.com>
2020-05-18 02:53:56 +0700

    Add Animations LandingPage

--- src/parts/Testimonial.js
+++ src/parts/Testimonial.js
@@ -1,53 +1,56 @@
 import React from "react";
 import Star from "elements/Star";
 import Button from "elements/Button";
+import Fade from 'react-reveal/Fade'
 
 export default function Testimonial({ data }) {
   return (
-    <section className="container">
-      <div className="row align-items-center">
-        <div className="col-auto" style={{ marginRight: 60 }}>
-          <div
-            className="testimonial-hero"
-            style={{ margin: `30px 0 0  30px` }}
-          >
-            <img
-              src={data.imageUrl}
-              className="position-absolute"
-              alt={data.name}
-              style={{ zIndex: 1 }}
-            />
-            <img
-              src={data.imageFrame}
-              className="position-absolute"
-              alt={data.name}
-              style={{ margin: "-30px 0 0 -30px" }}
-            />
-          </div>
-        </div>
-        <div className="col">
-          <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
-          <Star  value={data.rate} height={40} width={40} spacing={4}  />
-          <h5 className="h2 font-weight-light line-height-2 my-3">
-            {data.content}
-          </h5>
-          <span className="text-gray-500">
-            {data.familyName}, {data.familyOccupation}
-          </span>
-          <div>
-            <Button
-              type="link"
-              className="button-story btn px-5"
-              style={{ marginTop: 50}}
-              isOutline
-              hasShadow
-              href={`/testimonial/${data._id}`}
+    <Fade bottom>
+      <section className="container">
+        <div className="row align-items-center">
+          <div className="col-auto" style={{ marginRight: 60 }}>
+            <div
+              className="testimonial-hero"
+              style={{ margin: `30px 0 0  30px` }}
             >
+              <img
+                src={data.imageUrl}
+                className="position-absolute"
+                alt={data.name}
+                style={{ zIndex: 1 }}
+              />
+              <img
+                src={data.imageFrame}
+                className="position-absolute"
+                alt={data.name}
+                style={{ margin: "-30px 0 0 -30px" }}
+              />
+            </div>
+          </div>
+          <div className="col">
+            <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+            <Star value={data.rate} height={40} width={40} spacing={4} />
+            <h5 className="h2 font-weight-light line-height-2 my-3">
+              {data.content}
+            </h5>
+            <span className="text-gray-500">
+              {data.familyName}, {data.familyOccupation}
+            </span>
+            <div>
+              <Button
+                type="link"
+                className="button-story btn px-5"
+                style={{ marginTop: 50 }}
+                isOutline
+                hasShadow
+                href={`/testimonial/${data._id}`}
+              >
                 Read The Story
-            </Button>
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 }
